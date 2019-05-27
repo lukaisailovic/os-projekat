3bbdb2f3607080325e9dbfc5b7839cc757d65861
Luka Isailovic <lukaisailovic@gmail.com>
2019-05-27 16:48:01 +0200

    Extract keytools and encryption to their own files

--- linux-0.01/kernel/sys.c
+++ linux-0.01/kernel/sys.c
@@ -236,37 +236,7 @@ int sys_null(int nr)
 	return -ENOSYS;
 }
 
-/* Utils */
 
-
-static void __reverse(char *buf, int len)
-{
-	int i, j;
-	char c;
-	for(i = 0, j = len - 1; i < j; ++i, --j)
-	{
-		c = buf[i];
-		buf[i] = buf[j];
-		buf[j] = c;
-	}
-}
-
-int itoa(int n, char *buf)
-{
-	int i = 0, sign;
-	if((sign = n) < 0)
-		n = -n;
-	do
-	{
-		buf[i++] = n % 10 + '0';
-	} while((n /= 10) > 0);
-
-	if(sign < 0)
-		buf[i++] = '-';
-	buf[i] = '\0';
-	__reverse(buf, i);
-	return i;
-}
 
 /* OS2019 */
 extern long user_key_ptr, user_shift_ptr, user_alt_ptr;
@@ -274,96 +244,17 @@ extern int selected_layout, user_key_map_size;
 
 extern int sys_encr();
 
+/* start keytools */
+extern int sys_keygen();
+extern int sys_keyclear();
+extern int sys_keyshow();
+extern int sys_keyset();
+/* end keytools */
+
+/* start encryption */
 extern int sys_encrypt();
+/* end encryption */
 
-
-int r;
-int generate_random(int range_start, int range_end, int offset){
-	/* za random */
-	__asm__ __volatile__ (
-		"rdtsc;"
-		: "=a" (r)
-		:
-		: "%edx"
-	);
-	
-	__asm__ __volatile__ ( 
-		"movl (r), %%eax;"
-		"imull $7621, %%eax;"
-		"addl $1, %%eax;"
-		"movl $32768, %%ebx;"
-		"xorl %%edx, %%edx;"
-		"idivl %%ebx;"
-		"movl %%edx, (r);"
-		:
-		:
-		: "%eax", "%edx","%ebx","memory"
-	);
-	return ((r + offset) % (range_end - range_start)) + range_start;
-}
-
-int is_random_valid(const char *key,int key_length, int rand){
-	int i;
-	int valid = 1;
-	for(i = 0; i < key_length; i++){
-		if((*(key+i)) == rand){
-			valid = 0;
-			break;	
-		}
-	}
-	return valid;
-}
-
-int sys_keygen(int length, const char *buffer){
-	char key[17];
-	int i, rand;
-	
-	for(i = 0; i < length; i++){						
-		while(1){
-			rand = generate_random(33,126,i);		
-			if(is_random_valid(key,length,rand)){				
-				key[i] = rand;			
-				break;
-			} 		
-		}		
-	}
-	key[i+1] = 0;
-	printk(key);
-	printk("\n");
-	return 0;
-}
-
-char global_key[1024];
-
-int sys_keyset(int keylen,const char *key){
-	int i ;
-	for (i = 0; i < 1024;i++){
-		global_key[i] = get_fs_byte(key++);
-	}
-	//strcpy(&global_key,key);
-	//strcpy(&global_key,"projekat");
-	sys_keyshow();
-	return 0;
-	
-}
-
-int sys_keyclear(){
-	strcpy(&global_key,"");
-	printk("Global key cleared\n");
-	return 0;
-}
-
-int sys_keyshow(){
-	if(strlen(global_key) == 0){
-		printk("Global key is not set\n");
-	} else {
-		printk("Global key is set to: ");
-		printk(global_key);
-		printk("\n");
-	}
-	
-	return 0;
-}
 
 
 
